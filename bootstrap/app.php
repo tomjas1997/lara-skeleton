6238e640d0b18760a55da49abe847f7a19177c66
tomjas1997 <tomas.jasulaitis@invertus.eu>
2023-07-14 13:54:49 +0300

    Fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,10 +4,6 @@ if (file_exists(__DIR__.'/../vendor/autoload.php')) {
     require __DIR__.'/../vendor/autoload.php';
 }
 
-if (file_exists(__DIR__.'/../../../config/config.inc.php')) {
-    require __DIR__.'/../../../config/config.inc.php';
-}
-
 if (file_exists(__DIR__.'/../../../vendor/autoload.php')) {
     require __DIR__.'/../../../vendor/autoload.php';
 }
@@ -16,9 +12,6 @@ $app = new \Invertus\Laracore\Vendor\Foundation\Application\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
-$app->singleton(
-    Illuminate\Contracts\Console\Kernel::class,
-    $app->make(\Skeleton\Module\Console\Kernel::class)
-);
+$app->singleton(Illuminate\Contracts\Console\Kernel::class, \Skeleton\Module\Console\Kernel::class);
 
 return $app;
